24bb1d716ab69fdfb467a0d7e62044a300c29c11
tangting8806 <1426755050@qq.com>
2020-01-07 16:57:26 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/love/love_story/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.love.love_story
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
